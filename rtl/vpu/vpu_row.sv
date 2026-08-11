6ab03cb5fd19ade4f6262744eb3dd213f8766156
paulodietricheng <paulo.dietrich.eng@gmail.com>
2026-08-11 15:18:20 -0300

    add begining of softmax unit

--- rtl/vpu/vpu_row.sv
+++ rtl/vpu/vpu_row.sv
@@ -19,90 +19,180 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-//import fa_pkg::*;
+import fa_pkg::*;
 
-//module vpu_row (
-//    input logic clk, rst_n,
+module vpu_row (
+    input logic clk, rst_n,
 
-//    // MXU
-//    input logic mxu_done,
-//    input accumulator_t scores [0:SA_COLS-1],
-//    input accumulator_t row_max,
-//    input accumulator_t row_sum,
+    // MXU
+    input accumulator_t x_i,
+
+    // Control
+    input logic mxu_done,
+    input logic last_kv_tile,
+    output logic vpu_done,
+
+    // SRAM
+    input operand_t V_i [0:D_MODEL-1],
+    output operand_t O_N [0:D_MODEL-1]
+);
+
+    // Registers
+    accumulator_t m_i, m_i_minus_1;
+    accumulator_t d_i, d_i_minus_1;
+    accumulator_t o_i, o_i_minus_1;
+
+    // Intermediate signals
+    accumulator_t max_diff;
+    accumulator_t safe_diff;
+
+    accumulator_t exp_max_diff;
+    accumulator_t exp_safe_diff;
+
+    accumulator_t alpha;
+    accumulator_t beta;
+
+    // EXP units
+    exp U_EXP0 (
+        .in  (max_diff),
+        .out (exp_max_diff)
+    );
+
+    exp U_EXP1 (
+        .in  (safe_diff),
+        .out (exp_safe_diff)
+    );
     
-//    // Control
-//    input logic last_kv_tile,
-//    output logic done,
+    // Combinational datapath
+    assign max_diff  = m_i_minus_1 - m_i;
+    assign safe_diff = x_i - m_i;
 
-//    // SRAM
-//    input operand_t V_tile [0:SA_COLS-1][0:D_MODEL-1],
-//    output accumulator_t O_row [0:D_MODEL-1]
-//);
+    assign alpha = d_i_minus_1 * exp_max_diff;
+    assign beta  = exp_safe_diff;
 
-//    // Registers
-//    accumulator_t m_reg;
-//    accumulator_t l_reg;
-//    accumulator_t O_reg[0:D_MODEL-1];
+    // States
+    typedef enum logic [2:0] {
+        v_IDLE,
+        v_MAX,
+        v_SUM,
+        v_UPDATE,
+        v_DONE
+    } vpu_state_t;
+
+    vpu_state_t curr_state;
+
+    // Control flags
+    logic max_done;
+    logic sum_done;
+    logic update_done;
+
+    // FSM
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            // Reset state
+            curr_state <= v_IDLE;
+
+            // Reset registers
+            m_i         <= 32'hFF800000; // -infinity
+            m_i_minus_1 <= 32'hFF800000;
+
+            d_i         <= 32'h0;
+            d_i_minus_1 <= 32'h0;
+
+            o_i         <= 32'h0;
+            o_i_minus_1 <= 32'h0;
+
+            // Reset output/control
+            vpu_done <= 1'b0;
+        end else begin
+            case (curr_state)
+                // IDLE
+                v_IDLE: begin
+                    vpu_done <= 1'b0;
+                    if (mxu_done) begin
+                        curr_state <= v_MAX;
+                    end
+                end
+
+                // MAX
+                v_MAX: begin
+                    // m_i = max(m_i_minus_1, x_i)
+
+                    if (x_i > m_i_minus_1)
+                        m_i <= x_i;
+                    else
+                        m_i <= m_i_minus_1;
+
+                    if (max_done) begin
+                        curr_state <= v_SUM;
+                    end
+                end
+
+                // SUM
+                v_SUM: begin
+                    // Differences needed by exponential units
+                    //
+                    // max_diff  = m_(i-1) - m_i
+                    // safe_diff = x_i - m_i
+
+                    // d_i =
+                    //     d_(i-1) * exp(m_(i-1)-m_i)
+                    //     + exp(x_i-m_i)
+
+                    d_i <=
+                        d_i_minus_1 * exp_max_diff
+                        + exp_safe_diff;
+
+                    if (sum_done) begin
+                        curr_state <= v_UPDATE;
+                    end
+                end
+                // UPDATE
+                v_UPDATE: begin
+
+                    // Save previous values
+                    m_i_minus_1 <= m_i;
+                    d_i_minus_1 <= d_i;
+
+                    // ------------------------------------------------
+                    // Output update
+                    // ------------------------------------------------
+                    //
+                    // alpha =
+                    // d_(i-1) * exp(m_(i-1)-m_i)
+                    //
+                    // beta =
+                    // exp(x_i-m_i)
+                    //
+                    // o_i =
+                    // (alpha / d_i) * o_(i-1)
+                    // +
+                    // (beta / d_i) * V_i
+                    //
+
+                    if (update_done) begin
+                        o_i_minus_1 <= o_i;
+                        
+                        if (last_kv_tile) begin
+                            curr_state <= v_DONE;
+                        end
+                        else begin
+                            curr_state <= v_IDLE;
+                        end
+                    end
+                end
+
+                // DONE
+                v_DONE: begin
+                    vpu_done <= 1'b1;
+                    curr_state <= v_DONE;
+                end
+
+                default: begin
+                    curr_state <= v_IDLE;
+                end
+            endcase
+        end
+    end
     
-//    accumulator_t m_next;
-//    accumulator_t l_next;
-//    accumulator_t O_next[0:D_MODEL-1];
-
-//    logic update_done;
-
-//    vpu_softmax softmax (
-//        .clk(clk),
-//        .rst(rst),
-//        .valid(mxu_done),
-//        .m_old(m_reg),
-//        .l_old(l_reg),
-//        .row_max(row_max),
-//        .row_sum(row_sum),
-//        .m_new(m_next),
-//        .l_new(l_next),
-//        .alpha(alpha),
-//        .done(softmax_done)
-//    );
-
-
-//    vpu_accum accum (
-//        .clk(clk),
-//        .rst_n(rst_n),
-//        .valid(softmax_done),
-//        .alpha(alpha),
-//        .scores(scores),
-//        .V_tile(V_tile),
-//        .O_old(O_reg),
-//        .O_new(O_next),
-//        .done(accum_done)
-//    );
-
-
-//    always_ff @(posedge clk) begin
-//        if(!rst_n) begin
-//            m_reg <= '0;
-//            l_reg <= '0;
-            
-//            for(int i=0; i < D_MODEL; i++)
-//                O_reg[i] <= '0;
-                
-//            done <= 0;
-//        end else begin
-//            done <= 0;
-//            if(accum_done) begin
-
-//                m_reg <= m_next;
-//                l_reg <= l_next;
-
-//                for(int i=0;i<D_MODEL;i++)
-//                    O_reg[i] <= O_next[i];
-                    
-//                if(last_kv_tile)
-//                    done <= 1;
-//            end
-//        end
-//    end
-
-//    assign O_row = O_reg;
-
-//endmodule
+endmodule
