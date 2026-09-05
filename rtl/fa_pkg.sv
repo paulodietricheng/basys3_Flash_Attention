f4c55045d85043f6816da250de9c971b5f7b6be4
paulodietricheng <paulo.dietrich.eng@gmail.com>
2026-09-05 14:30:18 -0500

    branch dma and hbm files to create a pure accelerator

--- rtl/fa_pkg.sv
+++ rtl/fa_pkg.sv
@@ -21,101 +21,37 @@
 
 
 package fa_pkg;
-
-    // ------------------------------------------------------------
-    // Public architectural parameters (unify sram addressing space)
-    // ------------------------------------------------------------
-    localparam SA_COLS    = 8;
-    localparam SA_ROWS    = 8;
-    localparam D_MODEL    = 16;
     
-    // Model precision parameters
+    //=============================================================================
+    // Model Precision
+    //============================================================================
     localparam OPERAND_W  = 8; // INT8
     localparam ACC_W      = 32; // INT32
+    localparam D_MODEL    = 16;
 
     //=============================================================================
     // BRAM organization
-    //=============================================================================
-    
-    // Bram parameters
+    //============================================================================
+    localparam BRAM_PORT_W = 32;
     localparam NUM_PORTS = 2;
-    localparam BRAM_WORD_W = 32;
-    localparam BRAM_DEPTH  = 1024;
-    localparam WPA = BRAM_WORD_W / OPERAND_W;
-    
-    // Capacity of one BRAM primitive
-    localparam BRAM_BYTE_COUNT = BRAM_DEPTH * (BRAM_WORD_W / 8);
-    localparam BRAM_BC_W       = $clog2(BRAM_BYTE_COUNT);
+    localparam WPA = BRAM_PORT_W / OPERAND_W;
     
     //=============================================================================
-    // On-chip SRAM
-    //=============================================================================
+    // Buffer organization
+    //============================================================================
+    localparam NUM_BUF = 4;
     
-    localparam int NUM_BUF = 4;
-    localparam int DMA_CH  = 2;
-    
-    // Four BRAMs combined into one SRAM buffer
-    localparam SRAM_WORD_W = 32;
-    localparam SRAM_DEPTH  = NUM_BUF * BRAM_DEPTH;
-    localparam SRAM_ADDR_W = $clog2(BRAM_DEPTH);
-    
-    //=============================================================================
-    // Fake HBM organization
-    //=============================================================================
-    //
-    // The HBM model is composed of 16 BRAM primitives:
-    //
-    //   - 4 BRAMs : Query  (Q)
-    //   - 4 BRAMs : Key    (K)
-    //   - 4 BRAMs : Value  (V)
-    //   - 4 BRAMs : Output (O)
-    //
-    // Therefore each matrix occupies four BRAM primitives.
-    //=============================================================================
-    
-    localparam HBM_DEPTH      = 16 * BRAM_DEPTH;
-    localparam HBM_ADDR_W     = $clog2(HBM_DEPTH);
-    localparam HBM_BYTE_COUNT = HBM_DEPTH * (BRAM_WORD_W / 8);
-    
-    // Number of BRAMs allocated to each matrix
-    localparam BRAMS_PER_MATRIX = 4;
-    
-    // Storage available for a single matrix (Q, K, V or O)
-    localparam MATRIX_BYTE_COUNT = HBM_BYTE_COUNT / BRAMS_PER_MATRIX;
-    
-    //=============================================================================
-    // Tiling parameters
-    //=============================================================================
-    
-    // Storage required for one token (embedding vector)
-    localparam BYTES_PER_TOKEN = (D_MODEL * OPERAND_W) / 8;
-    
-    // Maximum sequence length that fits entirely in one matrix stored in HBM
-    localparam MAX_SQ_LEN = MATRIX_BYTE_COUNT / BYTES_PER_TOKEN;
-    localparam MAX_SQ_L_W = $clog2(MAX_SQ_LEN);
-    
-    // Maximum sequence length that fits in one BRAM tile
-    localparam MAX_TILE_SQ_LEN = BRAM_BYTE_COUNT / BYTES_PER_TOKEN;
-    localparam MAX_TSQ_L_W = $clog2(MAX_TILE_SQ_LEN);
-    
-    // Number of BRAM tiles required to process the largest sequence stored in HBM.
-    localparam MAX_TILE_COUNT = (MAX_SQ_LEN + MAX_TILE_SQ_LEN - 1) / MAX_TILE_SQ_LEN;
-    localparam MAX_TC_W = $clog2(MAX_TILE_COUNT);
-    
-    // Number of MXU subtiles (SA_ROWS tokens per subtile) required to process
-    // the largest BRAM tile.
-    localparam MAX_STILE_COUNT = (MAX_TILE_SQ_LEN + SA_ROWS - 1) / SA_ROWS;
-    localparam MAX_STC_W = $clog2(MAX_STILE_COUNT);
-    
-    // ------------------------------------------------------------
+    // ============================================================================
     // Systolic Array
-    // ------------------------------------------------------------
+    // ============================================================================
        
     // Input matrices bus widths
     
     // The maximum size of a result of a MxKxN GEMM is a MxN
     // matrix, thus, as the os systolic array produces at max 
     // an SA_SIZE x SA_SIZE matrix, 
+    localparam SA_COLS    = 8;
+    localparam SA_ROWS    = 8;
     
     localparam M_W = $clog2(SA_COLS + 1); // + 1 to range from 1
     localparam N_W = $clog2(SA_ROWS + 1); // to SA_SIZE.
@@ -144,43 +80,20 @@ package fa_pkg;
         n_dim_t c_n_offset;
         
     } mxu_cmd_t ;
-    
-    // Systolic latency parameter
-    localparam DSP_LAT = 4;
 
     // Total result latency from first PE token to final valid result.
     localparam RESULT_LAT_W = $clog2(2*SA_ROWS + SA_COLS + D_MODEL - 2);
 
-    // Scalar data types
-    typedef logic signed [OPERAND_W-1:0] operand_t;
-    typedef logic signed [ACC_W-1:0]     accumulator_t;
-    typedef logic [SRAM_WORD_W-1:0]      sram_word_t;
-    typedef logic [BRAM_WORD_W-1:0]      bram_word_t;
+    // ============================================================================
+    // Scalar datatypes
+    // ============================================================================
+    typedef logic signed [OPERAND_W-1:0]   operand_t;
+    typedef logic signed [ACC_W-1:0]       accumulator_t;
+    typedef logic        [BRAM_PORT_W-1:0] buf_port_t;
     
-    // DMA request
-    typedef struct packed {
-        logic direction; // 0 = HBM -> SRAM, 1 = SRAM -> HBM
-        logic transpose; // 0 = false, 1 = true
-        logic [1:0] buf_tag;
-        // 00 BUFA
-        // 01 BUFB
-        // 10 BUFC
-        // 11 BUFD
-        
-        logic [9:0] rows;
-        logic [9:0] cols;
-        
-        logic [BRAM_BC_W-1:0] byte_count;
-        
-        // Base addresses
-        logic [HBM_ADDR_W-1:0] hbm_base_addr;
-        logic [SRAM_ADDR_W-1:0] sram_base_addr;
-                
-        logic [HBM_ADDR_W-1:0] hbm_stride;        
-        
-    } dma_rq_t ;
-    
+    // ============================================================================
     // VPU
+    // ============================================================================
     localparam row_idx_w = $clog2(SA_ROWS);
     localparam colg_idx_w = $clog2(D_MODEL / (NUM_PORTS * WPA));
     localparam V_IDX_W = row_idx_w + colg_idx_w;
